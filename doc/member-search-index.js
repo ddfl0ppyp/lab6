1a63f3059ec71de8dc48a1b7c932575914832826
ddfl0ppyp <dontletthenightmaresbite@gmail.com>
2024-11-10 16:07:16 +0300

    init

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"edu.java.lab6","c":"RaceManager","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"edu.java.lab6","c":"RaceManager","l":"RaceManager()","u":"%3Cinit%3E()"},{"p":"edu.java.lab6","c":"RaceManager","l":"show()"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.java.lab6","l":"RaceManager"}];updateSearchResults();
